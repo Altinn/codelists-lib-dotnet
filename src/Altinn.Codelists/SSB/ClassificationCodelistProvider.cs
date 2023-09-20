fb1e6e1138bd8a95972f075b2465b7b8313e3571
Ronny Birkeli <ronny.birkeli@digdir.no>
2023-09-20 20:30:48 +0200

    Fix code smell

--- src/Altinn.Codelists/SSB/ClassificationCodelistProvider.cs
+++ src/Altinn.Codelists/SSB/ClassificationCodelistProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Altinn.App.Core.Features;
 using Altinn.App.Core.Models;
 using Altinn.Codelists.SSB.Models;
+using System.Globalization;
 using System.Linq;
 
 namespace Altinn.Codelists.SSB;
@@ -58,7 +59,7 @@ public class ClassificationCodelistProvider : IAppOptionsProvider
         Dictionary<string, string> mergedKeyValuePairs = MergeDictionaries(_defaultKeyValuePairs, keyValuePairs);
 
         string? date = mergedKeyValuePairs.GetValueOrDefault("date");
-        DateOnly dateOnly = date == null ? DateOnly.FromDateTime(DateTime.Today) : DateOnly.Parse(date);
+        DateOnly dateOnly = date == null ? DateOnly.FromDateTime(DateTime.Today) : DateOnly.Parse(date, DateTimeFormatInfo.InvariantInfo);
         string level = mergedKeyValuePairs.GetValueOrDefault("level") ?? string.Empty;
         string variant = mergedKeyValuePairs.GetValueOrDefault("variant") ?? string.Empty;
 
